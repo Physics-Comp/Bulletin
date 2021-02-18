a940cfa9fbf283e2ee455d3de697a4cc8c805f5e
BrandenEsses <branden.esses@colorado.edu>
2021-02-18 01:31:35 -0700

    Started authentication

--- bulletin.py
+++ bulletin.py
@@ -1,47 +1,75 @@
 from datetime import datetime
 from pytz import timezone
-from flask import Flask, request, json, Response, render_template
-import pymongo
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mongoengine import MongoEngine, Document
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import Email, Length, InputRequired
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["TASKS"]
-mycol = mydb["tasks"]
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'TASKS',
+    'host': 'mongodb://localhost:27017'
+}
+
+db = MongoEngine(app)
+app.config['SECRET_KEY'] = 'issacnewton'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Document):
+    meta = {'collection': 'authentication'}
+    email = db.StringField(max_length=30)
+    password = db.StringField()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id).first()
+
+class RegForm(FlaskForm):
+    email = StringField('email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
 
 @app.route('/')
 def base():
     return render_template("index.html")
 
-@app.route('/login')
-def base2():
-    return render_template("login.html")
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+    if request.method == 'POST':
+        if form.validate():
+            existing_user = User.objects(email=form.email.data).first()
+            if existing_user is None:
+                hashpass = generate_password_hash(form.password.data, method='sha256')
+                hey = User(form.email.data,hashpass).save()
+                login_user(hey)
+                return redirect(url_for('dashboard'))
+    return render_template('register.html', form=form)
 
-@app.route('/submit_credentials', methods=['POST'])
-def base3():
-	creds = dict(request.form)
-	return creds
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated == True:
+        return redirect(url_for('dashboard'))
+    form = RegForm()
+    if request.method == 'POST':
+        if form.validate():
+            check_user = User.objects(email=form.email.data).first()
+            if check_user:
+                if check_password_hash(check_user['password'], form.password.data):
+                    login_user(check_user)
+                    return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
 
-@app.route('/mongodb', methods=['POST'])
-def poster():
-	form_dict = dict(request.form)
-	fmt = '%Y-%m-%d %H:%M:%S'
-	now = datetime.now(timezone('MST'))
-	form_dict['date'] = now.strftime(fmt)
-	fmt2 = '%Y-%m-%d'
-	form_dict['day'] = now.strftime(fmt2)
-	mycol.insert_one(form_dict)
-	return render_template("create.html")
-
-@app.route('/date', methods=['POST'])
-def getter():
-	form_dict = dict(request.form)
-	if(form_dict["day"]==""):
-		results = mycol.find()
-		return render_template('read.html', results = results)
-	else:
-		results = mycol.find(form_dict)
-		return render_template('read.html', results = results)
+@app.route('/logout', methods = ['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555, host='0.0.0.0')
